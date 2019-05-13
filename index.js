4fbbe6e3536a69e549f51e146786b07ff6b6b0ec
MAllen07 <mrsmallen07@gmail.com>
2019-05-13 18:21:24 -0400

    Initiated project - impementing api in indexjs fle

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+
+//create server and run it 
+
+const express = require('express');
+
+//create the server
+const app = express();
+
+// tell server what port to listen on
+app.listen()
